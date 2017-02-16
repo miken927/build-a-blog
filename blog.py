f3901c994f1bec79bfce2f493de558931f7de76e
Mike Novak <miken927@hotmail.com>
2017-02-16 14:38:44 -0600

    first basic commit

---
+++ blog.py
@@ -0,0 +1,38 @@
+import os
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello Blog!')
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainBlog(webapp2.RequestHandler):
+    self.response.write('main blog here')
+
+
+class NewPost(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+    ('/blog', MainBlog)
+    ('/newpost',NewPost)
+], debug=True)
